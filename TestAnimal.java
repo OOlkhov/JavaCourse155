488201685ab17aea7214437832a3c015955498e5
Oleksii <olkhov07@gmail.com>
2022-10-02 15:29:32 +0300

    new object

--- TestAnimal.java
+++ TestAnimal.java
@@ -3,9 +3,11 @@ public class TestAnimal {
 	public static void main (String[] args) {
 		Animal animal1 = new Animal ("Snijok", "white");
 		Animal animal2 = new Animal ("Barsyk", "brown");
+		Animal animal3 = new Animal ("Felis silvestris", "Grey-brown-red");
 		
 		animal1.sayHello();
 		animal2.sayHello();
+		animal3.sayHello();
 	}
 }
 		
