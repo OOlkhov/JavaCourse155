cce0f77a9deaafd729759ecb04249d86ed996410
Oleksii <olkhov07@gmail.com>
2022-10-16 17:15:23 +0300

    06 interface comparable added

--- Cat.java
+++ Cat.java
@@ -1,10 +1,20 @@
 public class Cat extends Animal implements Pet, Comparable <Cat> {
 	
 	private boolean goesOutside;
+	private int weight;
 	
-	public Cat(String name, String color, boolean goesOutside) {
+	public Cat(String name, String color, boolean goesOutside, int w) {
 		super(name, color);
 		this.goesOutside = goesOutside;
+		weight = w;
+	}
+	
+	public int getWeight (){
+		return weight;
+	}
+	
+	public void setWeight (int w){
+		weight = w;
 	}
 		
 
@@ -44,7 +54,17 @@ public class Cat extends Animal implements Pet, Comparable <Cat> {
 		System.out.println("Here is my favourite toy, let's play");
 	}
 	
-	public Cat compareTo(Cat c){
-		
+	public String toString() {
+		return super.getName() + " " + super.getColor() + " " + this.getWeight();
+	}
 	
+	public int compareTo(Cat c) {
+		if (this.getWeight() == c.getWeight()) {
+			return 0;
+		} else if (this.getWeight() < c.getWeight()) {
+			return -1;
+		} else {
+			return 1;
+		}
+	}
 }
--- TestCat.java
+++ TestCat.java
@@ -1,9 +1,13 @@
+import java.util.Arrays;
+
 public class TestCat{
 	public static void main (String[] args) {
-		Cat cat1 = new Cat ("Murzik", "red", true);
+		Cat cat1 = new Cat ("Murzik", "red", true, 6);
 		cat1.makeNoise();
 		cat1.beFriendly();
 		cat1.play();
+		Cat cat2 = new Cat ("Bandit", "black", true, 5);
+		Cat cat3 = new Cat ("Felix", "green", true, 3);
 		System.out.println("=============================");
 		
 		Dog dog1 = new Dog("Sharik", "brown", true);
@@ -12,6 +16,12 @@ public class TestCat{
 		dog1.play();
 		dog1.tellJob();
 		dog1.serve();
+		
+		Cat cats[] = {cat1, cat2, cat3};
+		Arrays.sort(cats);
+		for (Cat cat : cats) {   
+			System.out.println(cat);
+		}
 		
 	}
 }
