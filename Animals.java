4b400e5eb4243f1d2a27cfef25b4bbe769cb7538
Oleksii <olkhov07@gmail.com>
2022-10-23 21:41:12 +0300

    08 2nd task

--- Animals.java
+++ Animals.java
@@ -1,34 +1,40 @@
 import java.util.*;
 
 public class Animals{
+	
+	private int count = 0;
+	private ArrayList<Animal> animals = new ArrayList<Animal>();
+	
+	public void addAnimal (Animal animal){
+		if (count < 10){
+			animals.add(animal);
+			count++;}
+		else {
+			System.out.println("List limit reached");
+		}
+	}
+	
+	public void printAnimals(){
+		System.out.println(animals);
+	}
+	
 	public static void main (String[] args) {
 		Cat cat1 = new Cat ("Murzik", "red", true, 6);
-		cat1.makeNoise();
-		cat1.beFriendly();
-		cat1.play();
 		Cat cat2 = new Cat ("Bandit", "black", true, 5);
 		Cat cat3 = new Cat ("Felix", "green", true, 3);
-		System.out.println("=============================");
 		
 		Dog dog1 = new Dog("Sharik", "brown", true, 8);
-		dog1.makeNoise();
-		dog1.beFriendly();
-		dog1.play();
-		dog1.tellJob();
-		dog1.serve();
 		Dog dog2 = new Dog ("Spot", "white", true, 5);
 		Dog dog3 = new Dog ("Fantik", "rgb", false, 3);
 		
-		System.out.println("=============================");
-		
-		ArrayList<Animal> animals = new ArrayList<Animal>();
-		animals.add(cat1);
-		animals.add(cat2);
-		animals.add(cat3);
-		animals.add(dog1);
-		animals.add(dog2);
-		animals.add(dog3);
-		System.out.println(animals);
+		Animals animals = new Animals();
+		animals.addAnimal(cat1);
+		animals.addAnimal(cat2);
+		animals.addAnimal(cat3);
+		animals.addAnimal(dog1);
+		animals.addAnimal(dog2);
+		animals.addAnimal(dog3);
+		animals.printAnimals();
 		
 		
 		
